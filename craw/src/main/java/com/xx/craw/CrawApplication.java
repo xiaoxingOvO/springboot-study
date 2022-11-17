78dd22d1b2cc20119d513bee01c0bc336c9712a7
xiaoxing <793408755@qq.com>
2022-11-17 14:10:43 +0800

    Craw初始化

--- craw/src/main/java/com/xx/craw/CrawApplication.java
+++ craw/src/main/java/com/xx/craw/CrawApplication.java
@@ -1,31 +1,57 @@
 package com.xx.craw;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+
 
 @SpringBootApplication
 public class CrawApplication {
 
-    public static void main(String[] args) throws UnknownHostException {
+    private static final Logger log = LoggerFactory.getLogger(CrawApplication.class);
+
+
+    public static void main(String[] args){
         ConfigurableApplicationContext application = SpringApplication.run(CrawApplication.class, args);
-//        Environment env = application.getEnvironment();
-//        String ip = InetAddress.getLocalHost().getHostAddress();
-//        String port = env.getProperty("server.port");
-//        String property = env.getProperty("server.servlet.context-path");
-//        String path = property == null ? "" : property;
-//
-//        System.out.println(
-//                "\n" +
-//                        "----------------------------------------------------------\n\t" +
-//                        "Application is running! Access URLs:\n\t" +
-//                        "Local: \t\thttp://localhost:" + port + path + "\n\t" +
-//                        "External: \thttp://" + ip + ":" + port + path + "\n" +
-//                        "----------------------------------------------------------\n");
+        Environment env = application.getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath);
     }
 
 }
