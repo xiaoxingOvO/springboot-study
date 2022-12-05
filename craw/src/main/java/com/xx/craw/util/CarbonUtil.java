d287194ec580cb3eacea9515764f86e9bd10ec35
xiaoxing <793408755@qq.com>
2022-12-05 17:46:35 +0800

    craw部分-已完成

--- craw/src/main/java/com/xx/craw/util/CarbonUtil.java
+++ craw/src/main/java/com/xx/craw/util/CarbonUtil.java
@@ -10,10 +10,14 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -29,8 +33,11 @@ import java.util.stream.Collectors;
 @Service
 public class CarbonUtil {
 
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(CarbonUtil.class);
+
     @Autowired
     private MarketTransactionService marketTransactionService;
+
 
     /**
      * 爬取碳交易每日数据
@@ -38,7 +45,9 @@ public class CarbonUtil {
      */
     public ResponseEntity<String> dailyTransactionData() {
         try {
-            for (int i = 1; i < 15; i++) {
+//            int pageNum = getPageNum();
+//            log.info("总共有"+pageNum+"页");
+            for (int i = 1; i < 16; i++) {
                 //1.打开浏览器，HTTPClient对象
                 CloseableHttpClient httpClient = HttpClients.createDefault();
                 //2.输入网址,发起get请求创建HTTPGet对象
@@ -55,20 +64,17 @@ public class CarbonUtil {
                 //4.解析响应，获取数据，
                 //判断状态码是否是200
                 if (response.getStatusLine().getStatusCode() == 200) {
-                    System.out.println("----------------------------------第" + i + "页--------------------------------------");
+                    log.info("----------------------------------第" + i + "页--------------------------------------");
 
                     HttpEntity httpEntity = response.getEntity();
                     String content = EntityUtils.toString(httpEntity, "utf8");
-//                    log.info(content);
-                    // 处理字符串,截取出右侧每日成交列表
-                    String strDays = StringUtils.substringBetween(content, "<ul class=\"list-unstyled articel-list\">", "</ul>");
-                    //截取每一个li标签，遍历获取每天的href
-                    String[] strDayLi = strDays.split("</li>");
+                    // Jsoup解析html
+                    Document document = Jsoup.parse(content);
+                    Elements elements = document.select("div.right-con > ul > li.text-ellipsis > a[href]");
+                    // 遍历获取每天的href
                     List<String> list = new ArrayList<>();
-                    for (String s : strDayLi) {
-                        if (s.contains("<span class=\"pull-right m-l-sm\">")) {
-                            list.add(StringUtils.substringBetween(s, "href=\"", "\""));
-                        }
+                    for (Element e : elements){
+                        list.add(e.attr("href"));
                     }
                     list.stream().map(s -> {
                         //得到每一天的时间
@@ -86,97 +92,40 @@ public class CarbonUtil {
                             CloseableHttpResponse dayResponse = httpClient.execute(dayDateUrl);
                             //解析响应，获取每一天的数据
                             if (dayResponse.getStatusLine().getStatusCode() == 200) {
+                                //将html转换成string
                                 HttpEntity dayHttpEntity = dayResponse.getEntity();
                                 String dayContent = EntityUtils.toString(dayHttpEntity, "utf8");
-                                System.out.println("第" + day + "详情");
-                                //先截取出信息所在的div标签
-                                String strDayDiv = StringUtils.substringBetween(dayContent, "<div class=\"article-con font16\">", "</dvi>");
-//                                System.out.println(strDayDiv);
-                                //根据p标签进行切割
-                                String[] strDayP = strDayDiv.split("</p>");
+
+                                log.info("开始爬取第"+day+"详情");
+                                //用Jsoup解析html
+                                Document document2 = Jsoup.parse(dayContent);
                                 List<String> allMaths = new ArrayList();
-                                //对含有目标信息的p进行切割
-                                Arrays.stream(strDayP).map(p -> {
-                                    if (p.contains("今日全国碳市场碳排放配额（CEA）挂牌协议交易")) {
-
-                                        if (p.contains("<span style=\"font-family: 宋体, SimSun; font-size: 16px; text-indent: 2em;\">")) {
-                                            String between = StringUtils.substringBetween(p, "<span style=\"font-family: 宋体, SimSun; font-size: 16px; color: rgb(0, 0, 0);\">", "</span>");
-                                            allMaths.add(between);
-                                        } else {
-                                            String str1 = p.substring(p.indexOf(">") + 1, p.length());
-                                            String str2 = str1.substring(str1.indexOf(">") + 1, str1.indexOf("<", str1.indexOf("<") + 1));
-                                            allMaths.add(str2);
-                                        }
-                                    }
-                                    if (p.contains("截至今日")) {
-                                        String str3 = p.substring(p.indexOf(">") + 1, p.length());
-                                        String str4 = str3.substring(str3.indexOf(">") + 1, str3.indexOf("<", str3.indexOf("<") + 1));
-                                        allMaths.add(str4);
-                                    }
-                                    return null;
-                                }).collect(Collectors.toList());
-
-                                String[] array = allMaths.toArray(new String[2]);
+                                //css选择器匹配对应信息
+                                Elements dailyDetail = document2.select("div.article-con > p:matches(今日全国碳市场碳排放配额（CEA）挂牌协议交易)");
+                                for (Element e:dailyDetail) {
+                                    allMaths.add(e.text());
+                                }
+                                Elements dailyDetailTwo = document2.select("div.article-con > p:matches(截至今日，全国碳市场碳排放配额（CEA）)");
+                                for (Element e:dailyDetailTwo){
+                                    allMaths.add(e.text());
+                                }
+                                //通过句号分割成四句话
+                                String[] array = allMaths.size() != 2 ? new String[2] : allMaths.toArray(new String[2]);
                                 String[] strDayDetail = (array[0] + array[1]).split("。");
-
+                                //取值
                                 if (strDayDetail.length == 4) {
-                                    //获取挂牌协议交易数据
-                                    MarketTransactionDTO marketTransactionDTO = new MarketTransactionDTO();
-                                    //处理第一句(type=0)
-                                    if (strDayDetail[0].contains("成交量")) {
-                                        BigDecimal volume = new BigDecimal(StringUtils.substringBetween(strDayDetail[0], "成交量", "吨").replace(",", ""));
-                                        marketTransactionDTO.setVolume(volume);
-                                    }
-                                    if (strDayDetail[0].contains("成交额")) {
-                                        BigDecimal turnover = new BigDecimal(StringUtils.substringBetween(strDayDetail[0], "成交额", "元").replace(",", ""));
-                                        marketTransactionDTO.setTurnover(turnover);
-                                    }
-                                    if (strDayDetail[0].contains("开盘价")) {
-                                        BigDecimal openingPrice = new BigDecimal(StringUtils.substringBetween(strDayDetail[0], "开盘价", "元").replace(",", ""));
-                                        marketTransactionDTO.setOpeningPrice(openingPrice);
-                                    }
-                                    if (strDayDetail[0].contains("最高价")) {
-                                        BigDecimal highestPrice = new BigDecimal(StringUtils.substringBetween(strDayDetail[0], "最高价", "元").replace(",", ""));
-                                        marketTransactionDTO.setHighestPrice(highestPrice);
-                                    }
-                                    if (strDayDetail[0].contains("最低价")) {
-                                        BigDecimal lowestPrice = new BigDecimal(StringUtils.substringBetween(strDayDetail[0], "最低价", "元").replace(",", ""));
-                                        marketTransactionDTO.setLowestPrice(lowestPrice);
-                                    }
-                                    if (strDayDetail[0].contains("收盘价")) {
-                                        BigDecimal closingPrice = new BigDecimal(StringUtils.substringBetween(strDayDetail[0], "收盘价", "元").replace(",", ""));
-                                        marketTransactionDTO.setClosingPrice(closingPrice);
-                                    }
-
-                                    //处理第四句
-                                    if (strDayDetail[3].contains("累计成交量")) {
-                                        BigDecimal totalVolume = new BigDecimal(StringUtils.substringBetween(strDayDetail[3], "成交量", "吨").replace(",", ""));
-                                        marketTransactionDTO.setTotalVolume(totalVolume);
-                                    }
-                                    if (strDayDetail[3].contains("累计成交额")) {
-                                        BigDecimal totalTurnover = new BigDecimal(StringUtils.substringBetween(strDayDetail[3], "成交额", "元").replace(",", ""));
-                                        marketTransactionDTO.setTotalTurnover(totalTurnover);
-                                    }
-                                    marketTransactionDTO.setDate(LocalDate.parse(day));
-                                    marketTransactionDTO.setType(0);
-
+                                    // 获取挂牌交易数据
+                                    MarketTransactionDTO marketTransactionDTO = getMarketTransactionDTO(day, strDayDetail);
                                     System.out.println("挂牌交易对象：  " + marketTransactionDTO);
                                     // 插入挂牌交易数据
 //                                    marketTransactionService.insertMarketForCraw(marketTransactionDTO);
-
-                                    //获取大宗协议交易，处理第二句
-                                    if (strDayDetail[1].contains("成交量") || strDayDetail[1].contains("成交额")) {
-                                        MarketTransactionDTO marketTransactionDTO1 = new MarketTransactionDTO();
-                                        marketTransactionDTO1.setVolume(new BigDecimal(StringUtils.substringBetween(strDayDetail[1], "成交量", "吨").replace(",", "")));
-                                        marketTransactionDTO1.setTurnover(new BigDecimal(StringUtils.substringBetween(strDayDetail[1], "成交额", "元").replace(",", "")));
-                                        marketTransactionDTO1.setDate(LocalDate.parse(day));
-                                        marketTransactionDTO1.setType(1);
-
-                                        // 插入大宗交易数据
-                                        System.out.println("大宗交易对象：  "+marketTransactionDTO1);
+                                    //获取大宗协议交易
+                                    MarketTransactionDTO marketTransactionDTO2 = getMarketTransactionDTO2(day, strDayDetail);
+                                    // 插入大宗交易数据
+                                    System.out.println("大宗交易对象：  "+marketTransactionDTO2);
 //                                        marketTransactionService.insertMarketForCraw(marketTransactionDTO1);
-
-                                    }
+                                }else {
+                                    log.info("-------------------第"+day+"爬取失败！！！");
                                 }
 
                             }
@@ -215,6 +164,8 @@ public class CarbonUtil {
             if (response.getStatusLine().getStatusCode() == 200) {
                 HttpEntity httpEntity = response.getEntity();
                 String content = EntityUtils.toString(httpEntity, "utf8");
+                System.out.println(content);
+
                 //切割字符串
                 String[] strs = content.split("],");
                 //对数组中每个字符串进行处理
@@ -229,19 +180,10 @@ public class CarbonUtil {
 
                 //遍历lists入库
                 lists.stream().map(s -> {
-                    String[] array = s.toArray(new String[9]);
-                    MarketTradeDetailsDTO marketTradeDetailsDTO = new MarketTradeDetailsDTO();
-                    // TODO market_id值的设置
-                    marketTradeDetailsDTO.setCurrentTime(array[0]);//当前时间
-                    marketTradeDetailsDTO.setDate(LocalDate.now());//日期
-                    marketTradeDetailsDTO.setLatestPrice(new BigDecimal(array[1].replace(",", "")));//最新价
-                    marketTradeDetailsDTO.setAvgTradePrice(new BigDecimal(array[5].replace(",", "")));//成交均价
-                    marketTradeDetailsDTO.setVolume(new BigDecimal(array[6].replace(",", "")));//成交量
-                    marketTradeDetailsDTO.setTurnover(new BigDecimal(array[7].replace(",", "")));//成交金额
-                    marketTradeDetailsDTO.setQuoteChange(new BigDecimal(StringUtils.stripEnd(array[8], "%\"]")));//涨跌幅
+                    MarketTradeDetailsDTO marketTradeDetailsDTO = getMarketTradeDetails(s);
                     //插入
-                    marketTransactionService.insertMarketDetailsForCraw(marketTradeDetailsDTO);
-
+//                    marketTransactionService.insertMarketDetailsForCraw(marketTradeDetailsDTO);
+                    //输出
 //                    System.out.println("每分钟交易详情：   "+marketTradeDetailsDTO);
                     return null;
                 }).collect(Collectors.toList());
@@ -253,4 +195,110 @@ public class CarbonUtil {
 
         return ResponseEntity.ok(null);
     }
+
+    //每分钟交易数据
+    private MarketTradeDetailsDTO getMarketTradeDetails(List<String> s) {
+        String[] array = s.toArray(new String[9]);
+        MarketTradeDetailsDTO marketTradeDetailsDTO = new MarketTradeDetailsDTO();
+        // TODO market_id值的设置
+        marketTradeDetailsDTO.setCurrentTime(array[0]);//当前时间
+        marketTradeDetailsDTO.setDate(LocalDate.now());//日期
+        marketTradeDetailsDTO.setLatestPrice(new BigDecimal(array[1].replace(",", "")));//最新价
+        marketTradeDetailsDTO.setAvgTradePrice(new BigDecimal(array[5].replace(",", "")));//成交均价
+        marketTradeDetailsDTO.setVolume(new BigDecimal(array[6].replace(",", "")));//成交量
+        marketTradeDetailsDTO.setTurnover(new BigDecimal(array[7].replace(",", "")));//成交金额
+        marketTradeDetailsDTO.setQuoteChange(new BigDecimal(StringUtils.stripEnd(array[8], "%\"]")));//涨跌幅
+        return marketTradeDetailsDTO;
+    }
+
+
+    //大宗协议交易
+    private MarketTransactionDTO getMarketTransactionDTO2(String day, String[] strDayDetail) {
+        MarketTransactionDTO marketTransactionDTO = new MarketTransactionDTO();
+        if (strDayDetail[1].contains("成交量") || strDayDetail[1].contains("成交额")) {
+            marketTransactionDTO.setVolume(new BigDecimal(StringUtils.substringBetween(strDayDetail[1], "成交量", "吨").replace(",", "")));
+            marketTransactionDTO.setTurnover(new BigDecimal(StringUtils.substringBetween(strDayDetail[1], "成交额", "元").replace(",", "")));
+            marketTransactionDTO.setDate(LocalDate.parse(day));
+            marketTransactionDTO.setType(1);
+        }
+        return marketTransactionDTO;
+    }
+
+    //挂牌交易数据
+    private MarketTransactionDTO getMarketTransactionDTO(String day, String[] strDayDetail) {
+        //获取挂牌协议交易数据
+        MarketTransactionDTO marketTransactionDTO = new MarketTransactionDTO();
+        //处理第一句(type=0)
+        if (strDayDetail[0].contains("成交量")) {
+            BigDecimal volume = new BigDecimal(StringUtils.substringBetween(strDayDetail[0], "成交量", "吨").replace(",", ""));
+            marketTransactionDTO.setVolume(volume);
+        }
+        if (strDayDetail[0].contains("成交额")) {
+            BigDecimal turnover = new BigDecimal(StringUtils.substringBetween(strDayDetail[0], "成交额", "元").replace(",", ""));
+            marketTransactionDTO.setTurnover(turnover);
+        }
+        if (strDayDetail[0].contains("开盘价")) {
+            BigDecimal openingPrice = new BigDecimal(StringUtils.substringBetween(strDayDetail[0], "开盘价", "元").replace(",", ""));
+            marketTransactionDTO.setOpeningPrice(openingPrice);
+        }
+        if (strDayDetail[0].contains("最高价")) {
+            BigDecimal highestPrice = new BigDecimal(StringUtils.substringBetween(strDayDetail[0], "最高价", "元").replace(",", ""));
+            marketTransactionDTO.setHighestPrice(highestPrice);
+        }
+        if (strDayDetail[0].contains("最低价")) {
+            BigDecimal lowestPrice = new BigDecimal(StringUtils.substringBetween(strDayDetail[0], "最低价", "元").replace(",", ""));
+            marketTransactionDTO.setLowestPrice(lowestPrice);
+        }
+        if (strDayDetail[0].contains("收盘价")) {
+            BigDecimal closingPrice = new BigDecimal(StringUtils.substringBetween(strDayDetail[0], "收盘价", "元").replace(",", ""));
+            marketTransactionDTO.setClosingPrice(closingPrice);
+        }
+        //处理第四句
+        if (strDayDetail[3].contains("累计成交量")) {
+            BigDecimal totalVolume = new BigDecimal(StringUtils.substringBetween(strDayDetail[3], "成交量", "吨").replace(",", ""));
+            marketTransactionDTO.setTotalVolume(totalVolume);
+        }
+        if (strDayDetail[3].contains("累计成交额")) {
+            BigDecimal totalTurnover = new BigDecimal(StringUtils.substringBetween(strDayDetail[3], "成交额", "元").replace(",", ""));
+            marketTransactionDTO.setTotalTurnover(totalTurnover);
+        }
+        marketTransactionDTO.setDate(LocalDate.parse(day));
+        marketTransactionDTO.setType(0);
+        return marketTransactionDTO;
+    }
+
+
+    public int getPageNum(){
+        int i = 15;
+        //爬取每分钟市场行情数据
+        try {
+            //1.打开浏览器，HTTPClient对象
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            //2.输入网址,发起get请求创建HTTPGet对象
+            HttpGet httpGet = null;
+            httpGet = new HttpGet("https://www.cneeex.com/qgtpfqjy/mrgk/2022n/index.shtml");
+            //3.按回车，发起请求，返回响应
+            CloseableHttpResponse response = httpClient.execute(httpGet);
+            //4.解析响应，获取数据，
+            //判断状态码是否是200
+            if (response.getStatusLine().getStatusCode() == 200) {
+                HttpEntity httpEntity = response.getEntity();
+                String content = EntityUtils.toString(httpEntity, "utf8");
+                Document document = Jsoup.parse(content);
+                Elements elements = document.select("div.pageBar td:matches(页)");
+                for (Element e:elements){
+                    i = Integer.valueOf(StringUtils.substringBetween(e.text(), "/", "页"));
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return i;
+    }
+
+
+
+
+
 }
