97c629c10e4f53a5783aaa4aab7cf3aa97ad5346
xiaoxing <793408755@qq.com>
2022-12-26 14:26:09 +0800

    12.26

---
+++ springboot-easyexcel/src/main/java/com/xx/easyexcel/config/Swagger3Config.java
@@ -0,0 +1,43 @@
+package com.xx.easyexcel.config;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author xx
+ * @date 2022/12/13
+ */
+
+@Configuration
+@EnableOpenApi
+public class Swagger3Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .groupName("1.0")
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("easyexcel RestFul APIs")
+                .description("easyexcel接口文档")
+                .contact(new Contact("xx", "http://blog.csdn.net", "xx@email.com"))
+                .version("1.0")
+                .build();
+    }
+}
